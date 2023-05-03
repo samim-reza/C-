1a2c34be683586544429eae2d398d6ad7ab4c1f9
samim101 <samimreza2111@gmail.com>
2023-05-04 03:18:58 +0600

    init

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define Test int t;cin>>t;while(t--)
+#define For for(int i=0; i<n; i++)
+#define endl "\n"
+#define vector vector<int>
+using namespace std;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("/home/samim101/input.txt", "r", stdin);
+    freopen("/home/samim101/output.txt", "w", stdout);
+    #endif
+    /*ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);*/
+}
